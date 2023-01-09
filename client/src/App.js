7c102ddb958b96c5c8dc2d5086b80c6e0ac047f6
Pedro Aristigueta <aristiguetam97@gmail.com>
2023-01-09 17:17:42 -0500

    prueba de backend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import DetailCountries from "./components/DetailCountries/DetailCountries";
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import axios from "axios";
-axios.defaults.baseURL = `http://localhost:3001/`;
+// axios.defaults.baseURL = `http://localhost:3001/`;
+axios.defaults.baseURL = `https://picountriesdeploy-production.up.railway.app/`;
 
 function App() {
   return (
