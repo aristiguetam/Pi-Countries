c69215c289333477a85104ce83d8facf137fa3c8
Pedro Aristigueta <aristiguetam97@gmail.com>
2023-01-10 12:04:22 -0500

    nueva actualizacion1.4

--- api/src/app.js
+++ api/src/app.js
@@ -17,8 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "*"
-    // "https://pi-countries-deploy-seven.vercel.app"
+    "https://pi-countries-deploy-seven.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
