d272ce7844db0465bdbb503ec8bf4d507e19270e
aristiguetam <aristiguetam97@gmail.com>
2023-02-18 13:27:46 -0500

    actualizacion deploy 2

--- api/src/db.js
+++ api/src/db.js
@@ -18,6 +18,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } =
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
 const basename = path.basename(__filename);
